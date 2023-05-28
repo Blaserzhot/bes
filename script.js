58dff6eac615d46ce4802876dc812373a86c1155
Blaserzhot <66004785+Blaserzhot@users.noreply.github.com>
2023-05-27 19:59:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+
+//Contact Page Validation
+function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+
+    var emailRegex = /^[^\\\\s@]+@[^\\\\s@]+\\\\.[^\\\\s@]+$/;
+
+    if (emailRegex.test(email) && message != "") {
+      alert("Form submitted successfully!");
+    } else {
+      alert("Please enter a valid email address and message.");
+    }
+  }
+
+
+
+//slideup + AJAX
+$(document).ready(function(){
+    $("#box1").click(function(){
+        $("main").load("about.html")
+		$("#backdrop").slideUp("slow", function(){
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#box2").click(function(){
+        $("main").load("projects.html")
+		$("#backdrop").slideUp("slow", function(){
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#box3").click(function(){
+        $("main").load("hobbies.html")
+		$("#backdrop").slideUp("slow", function(){
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#box4").click(function(){
+        $("main").load("contactme.html")
+		$("#backdrop").slideUp("slow", function(){
+        });
+    });
+});
+
+//AJAX
+$(document).ready(function(){
+    $("#page1").click(function(){
+        $("#content").load("about.html")
+    });
+});
+
+$(document).ready(function(){
+    $("#page2").click(function(){
+        $("#content").load("projects.html")
+    });
+});
+
+$(document).ready(function(){
+    $("#page3").click(function(){
+        $("#content").load("hobbies.html")
+    });
+});
+
+$(document).ready(function(){
+    $("#page2").click(function(){
+        $("#content").load("projects.html")
+    });
+});
+
+//Fixed header after splash page
+window.onscroll = function() {myFunction()};
+var header = document.getElementById("heading");
+var sticky = header.offsetTop;
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+	header.style.transition = "all 0.5s ease";
+  } else {
+    header.style.transition = "all 0.1s ease";
+	header.classList.remove("sticky");
+  }
+}
+
+
+//Arrow Scrolldown
+function scrollDown() {
+  window.scroll({
+    top: 1100,
+    behavior: 'smooth'
+  });
+}
+	
+
+
+
+
